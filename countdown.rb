93cfac78c4975efad0001778b61dc29f5577d299
Brandon Nightingale <brandon@jbnight.com>
2020-05-07 04:02:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(number)
-  while number > 1
+  while number > 0
     puts "#{number} SECONDS!"
     number -= 1
+    puts "HAPPY NEW YEAR!"
   end
-  puts "HAPPY NEW YEAR!"
 end
